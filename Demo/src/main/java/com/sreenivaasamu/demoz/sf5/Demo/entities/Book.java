95cfcea5de0647677f23b35b9c955b5a8dd62981
Sreenu Gopala <sreenugopala007@outlook.com>
2018-02-04 12:10:08 +0530

    Baseline project

---
+++ Demo/src/main/java/com/sreenivaasamu/demoz/sf5/Demo/entities/Book.java
@@ -0,0 +1,130 @@
+package com.sreenivaasamu.demoz.sf5.Demo.entities;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long bookId;
+
+	private String title;
+	private String isbn;
+	private Long copiesSold;
+	
+	@ManyToMany
+	private Set<Author> authors = new HashSet<>();
+
+	public Book(String title, String isbn, Set<Author> authors) {
+		super();
+		this.title = title;
+		this.isbn = isbn;
+		this.authors = authors;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public Long getCopiesSold() {
+		return copiesSold;
+	}
+
+	public void setCopiesSold(Long copiesSold) {
+		this.copiesSold = copiesSold;
+	}
+
+	public Set<Author> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(Set<Author> authors) {
+		this.authors = authors;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((authors == null) ? 0 : authors.hashCode());
+		result = prime * result + ((copiesSold == null) ? 0 : copiesSold.hashCode());
+		result = prime * result + ((isbn == null) ? 0 : isbn.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (authors == null) {
+			if (other.authors != null)
+				return false;
+		} else if (!authors.equals(other.authors))
+			return false;
+		if (copiesSold == null) {
+			if (other.copiesSold != null)
+				return false;
+		} else if (!copiesSold.equals(other.copiesSold))
+			return false;
+		if (isbn == null) {
+			if (other.isbn != null)
+				return false;
+		} else if (!isbn.equals(other.isbn))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		final int maxLen = 5;
+		return String.format("Book [title=%s, isbn=%s, copiesSold=%s, authors=%s]", title, isbn, copiesSold,
+				authors != null ? toString(authors, maxLen) : null);
+	}
+
+	private String toString(Collection<?> collection, int maxLen) {
+		StringBuilder builder = new StringBuilder();
+		builder.append("[");
+		int i = 0;
+		for (Iterator<?> iterator = collection.iterator(); iterator.hasNext() && i < maxLen; i++) {
+			if (i > 0)
+				builder.append(", ");
+			builder.append(iterator.next());
+		}
+		builder.append("]");
+		return builder.toString();
+	}
+
+}
